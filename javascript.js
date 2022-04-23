3290b6dd11da1b2885acfe42cf28d48813c64a2b
willlari <riverskiing@gmail.com>
2022-04-23 12:21:30 -0400

    V2 first commit user Interface

--- javascript.js
+++ javascript.js
@@ -17,6 +17,7 @@ function playerPlay(){
     let lowerCasePlay = play.toLowerCase();
     return lowerCasePlay;
 }
+
 function playGame(computerPlay, playerPlay){
     let message = "YOU LOOSE!";
     if(computerPlay == playerPlay){
@@ -44,5 +45,55 @@ function game(repetition){
         playGame(computerPlay(), playerPlay());
     }
 }
+function newGame(){
+    playGame(computerPlay(), playerPlay());
+}
 
-game(5);
+//game(5)
+
+//TEstint DOM 
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = "This is some text";
+
+container.appendChild(content);
+
+const contentRed = document.createElement('p');
+contentRed.classList.add('contentRed');
+contentRed.textContent = "I am red text";
+contentRed.style.color = "red";
+container.appendChild(contentRed);
+
+const h3Blue = document.createElement('div');
+h3Blue.classList.add('h3Blue');
+h3Blue.textContent = "Blue H3";
+h3Blue.style.color = "blue";
+h3Blue.style.background = "red";
+container.appendChild(h3Blue);
+
+const division = document.createElement('div');
+division.style.background = "purple";
+
+const textDiv = document.createElement('h1');
+textDiv.textContent="je suis le textDiv"
+
+
+const pDiv = document.createElement('p');
+pDiv.textContent="je suis le sous texte de textDiv"
+division.appendChild(textDiv);
+division.appendChild(pDiv);
+
+container.appendChild(division);
+
+
+
+//TESTING BUTTON NEW GAME
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', newGame);
+btn.style.background='red';
+
+
